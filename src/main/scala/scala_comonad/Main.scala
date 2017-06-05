danbroooks/fpinscala

Initial commit of project structure

src/main/scala/fpinscala/Main.scala
@@ -1,0 +1,5 @@
+package fpinscala
+
+object Main extends App {
+  println("Run the tests with sbt test")
+}

danbroooks/a-scala-comonad-tutorial

src/main/scala/scala_comonad/Main.scala
@@ -1,0 +1,5 @@
+package scala_comonad
+
+object Main extends App {
+  println("Run the tests with sbt test")
+}